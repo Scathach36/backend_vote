6cc252b58c8b3903f4690f5016048052379ae3c3
Scathach <scathach0822@hotmail.com>
2023-03-16 11:13:09 +0800

    优化登录传参

--- src/main/java/com/wechat/vote/controller/LoginController.java
+++ src/main/java/com/wechat/vote/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.wechat.vote.controller;
 
-import com.wechat.vote.entity.LoginEntity;
 import com.wechat.vote.entity.UserEntity;
 import com.wechat.vote.repository.UserEntityRepository;
 import com.wechat.vote.util.JwtUtil;
@@ -10,6 +9,8 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Api(tags = "登录相关接口")
 @RestController
@@ -30,26 +31,28 @@ public class LoginController {
 
     @ApiOperation("用户登录")
     @PostMapping("/login")
-    public LoginEntity login(@ApiParam("用户名")@RequestBody UserEntity user) {
+    public Map<String, Object> login(@ApiParam("用户名")@RequestBody UserEntity user) {
         UserEntity userEntity = userEntityRepository.findByUsername(user.getUsername());
+        Map<String, Object> json = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
         if (userEntity == null) {
-            LoginEntity loginInfo = new LoginEntity();
-            loginInfo.setCode("200");
-            loginInfo.setMsg("用户不存在");
-            return loginInfo;
+            json.put("code","200");
+            json.put("msg","用户不存在");
+            return json;
         }
         if (userEntity.getPassword().equals(user.getPassword())) {
-            LoginEntity loginInfo = new LoginEntity();
-            loginInfo.setCode("200");
-            loginInfo.setMsg("登录成功");
-            loginInfo.setToken(JwtUtil.createToken(userEntity.getUsername(), userEntity.getRole()));
-            loginInfo.setRole(userEntity.getRole());
-            return loginInfo;
+            json.put("code","200");
+            json.put("msg","登录成功");
+            data.put("token",JwtUtil.createToken(userEntity.getUsername(), userEntity.getRole()));
+            data.put("role",userEntity.getRole());
+            data.put("classNumber",userEntity.getClassNumber());
+            data.put("number",userEntity.getNumber());
+            json.put("data",data);
+            return json;
         } else {
-            LoginEntity loginInfo = new LoginEntity();
-            loginInfo.setCode("200");
-            loginInfo.setMsg("密码错误");
-            return loginInfo;
+            json.put("code","200");
+            json.put("msg","密码错误");
+            return json;
         }
     }
 }
